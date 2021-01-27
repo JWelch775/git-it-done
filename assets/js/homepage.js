934092757d26b68d1657d03d775b72d1c494e65e
Justin Welch <justin.welch075@gmail.com>
2021-01-27 11:48:35 -0600

    added function for fetching user info from github api

---
+++ assets/js/homepage.js
@@ -0,0 +1,15 @@
+function getUserRepos(user)
+    {
+        //format the githu api url
+        var apiUrl = "https://api.github.com/users/" + user +"/repos";
+
+        //make a request to the url
+        fetch(apiUrl).then(function(response)
+        {
+            response.json().then(function(data)
+            {
+                console.log(data);
+            });
+        });
+    };
+getUserRepos("JWelch775");
